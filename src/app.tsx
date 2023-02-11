4a0be9cc42be68b35fd6489bfc802ad489ce2bcc
Thomas King <102960825+tkgnm@users.noreply.github.com>
2023-02-11 01:08:12 +0000

    made a stave line

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,11 @@
-import { useState } from "preact/hooks";
-import preactLogo from "./assets/preact.svg";
 import "./app.css";
+import { Stave } from "./components/stave";
 
 export function App() {
   return (
     <>
       <div>Hello</div>
+      <Stave />
     </>
   );
 }
---
+++ src/components/stave.tsx
@@ -0,0 +1,5 @@
+import "./stave.css";
+
+export const Stave = () => {
+  return <div className="line"></div>;
+};
